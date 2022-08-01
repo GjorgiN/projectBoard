b0837344345b31c29046573bd32ad77666d916d8
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-08-01 17:42:34 +0200

    Adding home page

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,55 @@
+import LoginForm from "../Components/LoginForm";
+
+import { useState, useEffect } from "react";
+import MyProjects from "../Components/MyProjects";
+import axios from 'axios'
+
+const url = 'http://localhost:8080/api/auth/signin'
+
+const Home = ({isLoggedIn, setIsLoggedIn}) => {
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const data = {
+            username: e.target.username.value,
+            password: e.target.password.value
+        }
+
+        const config = {
+            data: data,
+            url: url,
+            method: 'post',
+        }
+
+        console.log(JSON.stringify(config));
+
+        try {
+            const res = await axios(config);
+            localStorage.setItem("user", res.data.token)
+            e.target.username.value = '';
+            e.target.password.value = '';
+            setIsLoggedIn(true);
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        const token = localStorage.getItem('user');
+        if (token) {
+            setIsLoggedIn(true);
+        }
+    }, [isLoggedIn])
+
+    console.log('isLoggedIn', isLoggedIn);
+
+    return (
+        <>
+            {isLoggedIn ? <MyProjects /> : <LoginForm handleSubmit={handleSubmit} />}
+        </>
+    )
+}
+
+export default Home
