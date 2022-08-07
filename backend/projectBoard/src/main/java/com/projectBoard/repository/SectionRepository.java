5af48b4a88e5a2718f45573e823f54b768c26927
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-08-07 23:00:27 +0200

    find sectionId by TaskId

--- backend/projectBoard/src/main/java/com/projectBoard/repository/SectionRepository.java
+++ backend/projectBoard/src/main/java/com/projectBoard/repository/SectionRepository.java
@@ -1,11 +1,15 @@
 package com.projectBoard.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.projectBoard.entity.Section;
 
 @Repository
 public interface SectionRepository extends JpaRepository<Section, Long>{
-
+	
+	@Query(value = "SELECT section_id FROM section_tasks WHERE tasks_id = :taskId", nativeQuery = true)
+	Long findSectionByTaskId(@Param("taskId") Long taskId);
 }
