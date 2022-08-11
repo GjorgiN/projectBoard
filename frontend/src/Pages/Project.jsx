cdbbabe4090559be29a05d9f867050b35198e79b
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-08-12 01:05:22 +0200

    close add section form when click outside the form

--- frontend/src/Pages/Project.jsx
+++ frontend/src/Pages/Project.jsx
@@ -49,6 +49,29 @@ const Project = ({ setIsLoggedIn, isLoggedIn }) => {
 
     }, [isLoaded])
 
+    const toggleShowAddSectionByClickingElsewhere = e => {
+        e.preventDefault();
+        e.stopImmediatePropagation()
+        if (e.target.id !== 'addSection') {
+            setShowAddSection(false);
+        }
+    }
+
+    useEffect(() => {
+
+        console.log('hola muchachos')
+        document.addEventListener('click', toggleShowAddSectionByClickingElsewhere)
+        return () => {
+            console.log('adios muchachos')
+            document.removeEventListener('click', toggleShowAddSectionByClickingElsewhere)
+        }
+
+    });
+
+    useEffect(() => {
+        if (showAddSection)
+            window.scrollBy({ left: 300, behavior: 'smooth' })
+    }, [showAddSection])
 
 
     return (
@@ -61,7 +84,7 @@ const Project = ({ setIsLoggedIn, isLoggedIn }) => {
                             <div className='d-flex'>
                                 {project.sectionsOrder.map(section => <Section setProject={setProject} project={project} projectId={project.id} tasks={project.tasks} section={project.sections[section]} key={section} />)}
                             </div>
-                            {!showAddSection && <Button onClick={() => setShowAddSection(!showAddSection)} className='d-flex justify-content-center align-items-center' style={{ width: '12rem', height: 'max-content' }} variant='light'>
+                            {!showAddSection && <Button id='addSection' onClick={() => { setShowAddSection(!showAddSection); }} className='d-flex justify-content-center align-items-center' style={{ width: '12rem', height: 'max-content' }} variant='light'>
                                 <img className='me-1' width='24rem' src={plusLg} />New Section</Button>}
                             {
                                 showAddSection && <AddNewSection baseUrl={baseUrl} project={project} setProject={setProject} showAddSection={showAddSection} setShowAddSection={setShowAddSection} />
