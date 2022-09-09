667fca7ad6d4aa47ca1c678508838fd0a5f1716d
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-09-09 23:59:00 +0200

    attachment response only title and id

---
+++ backend/projectBoard/src/main/java/com/projectBoard/request/AttachmentResponse.java
@@ -0,0 +1,16 @@
+package com.projectBoard.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class AttachmentResponse {
+
+	Long id;
+	String title;
+
+}
