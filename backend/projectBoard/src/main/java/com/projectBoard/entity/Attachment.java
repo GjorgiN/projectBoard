2e45da6a7e600da7c2b65ec99fca0b89a89ba91a
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-09-09 23:59:59 +0200

    adding lob annotation

--- backend/projectBoard/src/main/java/com/projectBoard/entity/Attachment.java
+++ backend/projectBoard/src/main/java/com/projectBoard/entity/Attachment.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -21,5 +22,7 @@ public class Attachment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	private String title;
+	@Lob
 	private byte[] attachment;
 }
