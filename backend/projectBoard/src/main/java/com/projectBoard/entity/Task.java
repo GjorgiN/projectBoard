5435cb93c4e30d80e194dde9307e120bb45642e5
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-09-09 23:59:36 +0200

    changing Lob to attachment lob goes in attachment

--- backend/projectBoard/src/main/java/com/projectBoard/entity/Task.java
+++ backend/projectBoard/src/main/java/com/projectBoard/entity/Task.java
@@ -7,7 +7,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -40,7 +39,6 @@ public class Task {
 	@ManyToOne
 	User assignedUser;
 	
-	@Lob
 	@OneToMany
 	Set<Attachment> attachments;
 }
