92df78e3899fab4301f37d16bb66e4a8fa3deb73
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-09-10 06:49:07 +0200

    not sure why this problems occured in first place

--- backend/projectBoard/src/main/java/com/projectBoard/entity/Attachment.java
+++ backend/projectBoard/src/main/java/com/projectBoard/entity/Attachment.java
@@ -1,11 +1,15 @@
 package com.projectBoard.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,9 +24,16 @@ import lombok.NoArgsConstructor;
 public class Attachment {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	
 	private String title;
+	
 	@Lob
+//	@Type(type="org.hibernate.type.BinaryType")
+	@Column(name = "attachment")
 	private byte[] attachment;
+	
+	@ManyToOne
+	Task task;
 }
