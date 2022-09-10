5fa9845098660beb4b01e51a9371562eab3e3f31
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-09-11 01:57:12 +0200

    adding buttons to delete and download file

--- frontend/src/Components/Attachments.jsx
+++ frontend/src/Components/Attachments.jsx
@@ -1,17 +1,25 @@
 import React, { useState } from 'react'
 import axios from 'axios';
+import trashCan from '../assets/trashCan.svg'
+import downloadFile from '../assets/downloadFile.svg'
 import { Popover, OverlayTrigger, Container, Row, Col, Form, Alert, Button } from 'react-bootstrap'
 
 const Attachments = ({ noAttachment, hasAttachment, project, setProject, task, baseUrl }) => {
-    const attachments = [...project.tasks[task.id].attachments] // make it state so update will be seen immediately
+    const [attachments, setAttachments] = useState([...project.tasks[task.id].attachments]);
     const [areFilesAttached, setAreFilesAttached] = useState(attachments.length > 0);
 
     const handleFilesAttachment = e => {
         e.preventDefault();
-        console.dir(e)
-        const formData = new FormData();
 
-        for (const file of e.target[0].files) {
+        const files = e.target[0].files;
+
+        if (files.length < 1) {
+            console.log('choose a file, you moron!')
+            return;
+        };
+
+        const formData = new FormData();
+        for (const file of files) {
             formData.append('attachments', file);
         }
 
@@ -55,13 +63,32 @@ const Attachments = ({ noAttachment, hasAttachment, project, setProject, task, b
 
 
     const popover = (
-        <Popover id="attachments">
+        <Popover style={{ minWidth: '23rem' }} id="attachments">
             <Popover.Header as="h3">Task's Attachments</Popover.Header>
             <Popover.Body>
                 <Container className='m-0 p-0'>
                     <Row className='m-0 mb-3 p-0'>
                         <Col className='m-0 p-0'>
-                            {areFilesAttached ? attachments.map(attachment => attachment.title) : <Alert className='p-1 m-0 mb-2 text-center' variant='info'>The task does not have attachments</Alert>}
+                            {areFilesAttached ?
+                                attachments.map(attachment => <Alert key={attachment.id} className='p-1 m-0 mb-2 text-start' variant='secondary'>
+                                    <Container className='m-0 p-0'>
+                                        <Row className='align-items-center m-0 p-0'>
+                                            <Col xs='8' className='m-0 p-0'>
+                                                <span className='m-0 p-0'>{attachment.title}</span>
+                                            </Col>
+                                            <Col className='d-flex justify-content-end m-0 p-0'>
+                                                <Button className='my-0 mx-1 p-1' size='sm' variant='outline-success'>
+                                                    <img height='22rem' src={downloadFile} />
+                                                </Button>
+                                                <Button className='m-0 p-1' size='sm' variant='outline-danger'>
+                                                    <img height='22rem' src={trashCan} />
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                    </Container>
+                                </Alert>)
+                                :
+                                <Alert className='p-1 m-0 mb-2 text-center' variant='info'>The task does not have attachments</Alert>}
                         </Col>
                     </Row>
                     <Row className='m-0 p-0'>
@@ -71,13 +98,13 @@ const Attachments = ({ noAttachment, hasAttachment, project, setProject, task, b
                                     <Container className='m-0 p-0'>
                                         <Row className='m-0 p-0'>
                                             <Col className='m-0 p-0'>
-                                                <Form.Label className='d-none'>Choose files to be attached to this task</Form.Label>
+                                                <Form.Label className='ms-1'>Choose files to attach to this task</Form.Label>
                                                 <Form.Control size='sm' type="file" multiple />
                                             </Col>
                                         </Row>
                                         <Row className='m-0 p-0'>
                                             <Col className='d-flex justify-content-end m-0 p-0'>
-                                                <Button className='mt-2' variant="secondary" size="sm" type='submit'>Submit</Button>
+                                                <Button className='mt-2' variant="secondary" size="sm" type='submit'>Add files</Button>
                                             </Col>
                                         </Row>
                                     </Container>
