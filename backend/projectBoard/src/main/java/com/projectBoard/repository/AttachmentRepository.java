a13fb9670f2268faa2f212fec6ff9550695d3baa
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-09-10 06:52:15 +0200

    Adding query get attachments per task by taskID

--- backend/projectBoard/src/main/java/com/projectBoard/repository/AttachmentRepository.java
+++ backend/projectBoard/src/main/java/com/projectBoard/repository/AttachmentRepository.java
@@ -1,11 +1,16 @@
 package com.projectBoard.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.projectBoard.entity.Attachment;
+import com.projectBoard.entity.Task;
 
 @Repository
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
-
+	@Query(value = "SELECT * FROM attachment WHERE task_id = :taskId", nativeQuery = true)
+	Set<Attachment> findAllByTask(Long taskId);
 }
